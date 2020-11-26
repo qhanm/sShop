0f2267a5834819f5635bcf307b3deb78e3bd8a80
quach hoai nam <nam.quach@dragonflyteam.com>
2020-11-26 18:01:42 +0700

    setup layout

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.layouts.master');
 });
 
 Route::get('reboot',function(){
